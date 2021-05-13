c3ddacde7bd4a61af513f8a0fcb87c69fbb4d057
Van Loc <tangvanloc8@gmail.com>
2021-05-13 15:43:05 +0700

    Chapter 7: Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root to: "static_pages#home"
     get "static_pages/home"
     get "static_pages/help"
+    resources :users
   end
 end
